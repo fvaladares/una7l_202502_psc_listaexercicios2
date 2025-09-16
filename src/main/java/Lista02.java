8a6dc29e52cdebca89d33f22454360eb9cd22933
Fabricio Valadares <fabricio.valadares@gmail.com>
2025-09-15 21:20:37 -0300

    Validação do número de tentativas máximo.

--- src/main/java/Lista02.java
+++ src/main/java/Lista02.java
@@ -1,3 +1,5 @@
+import exercicios.Exercicio010;
+
 import java.util.Scanner;
 
 public class Lista02 {
@@ -5,6 +7,10 @@ public class Lista02 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
+        Exercicio010 exercicio010 = new Exercicio010();
+
+        exercicio010.executar(input);
 
 
     }
--- src/main/java/exercicios/Exercicio010.java
+++ src/main/java/exercicios/Exercicio010.java
@@ -13,50 +13,80 @@ public class Exercicio010 {
         String senhaAdmin = "1234";
         String senhaAluno = "0000";
         String senhaProfessor = "4321";
+        boolean usuarioAutenticado = false;
+        int numeroTentativas = 0;
+        int limiteTentativas = 3;
 
-        System.out.println("SISTEMA DE LOGIN UNA");
-        System.out.println(); // Insere uma linha em branco
-
-        System.out.print("USUÁRIO: ");
-        nomeUsuario = input.next();
-
-        System.out.println();
-        System.out.print("SENHA: ");
-        senha = input.next();
-
-        // if aninhado
-        if (nomeUsuario.equalsIgnoreCase(usuarioAdmin)) {
-            if (senha.equals(senhaAdmin)) {
-                System.out.println();
-                System.out.println("Bem-vindo, administrador");
-            } else {
-                System.out.println();
-                System.out.println("Senha incorreta");
-            }
-        } else if (nomeUsuario.equalsIgnoreCase(usuarioAluno)) {
-            if (senha.equals(senhaAluno)) {
-                System.out.println();
-                System.out.println("Bem-vindo, aluno");
-            } else {
-                System.out.println();
-                System.out.println("Senha incorreta");
-            }
-        } else if (nomeUsuario.equalsIgnoreCase(usuarioProfessor)) {
-            if (senha.equals(senhaProfessor)) {
-                System.out.println();
-                System.out.println("Bem-vindo, professor");
-            } else {
-                System.out.println();
-                System.out.println("Senha incorreta");
-            }
-        } else {
+        while ((numeroTentativas < limiteTentativas)
+                && (usuarioAutenticado == false)) {
+            System.out.println("TENTATIVA " + numeroTentativas);
             System.out.println();
-            System.out.println("Usuário não encontrado");
+            System.out.println("SISTEMA DE LOGIN UNA");
+            System.out.println(); // Insere uma linha em branco
+
+            System.out.print("USUÁRIO: ");
+            nomeUsuario = input.next();
+
+            System.out.println();
+            System.out.print("SENHA: ");
+            senha = input.next();
+            // if aninhado
+            if (nomeUsuario.equalsIgnoreCase(usuarioAdmin)) {
+                if (senha.equals(senhaAdmin)) {
+                    System.out.println();
+                    System.out.println("Bem-vindo, administrador");
+                    usuarioAutenticado = true;
+                } else {
+                    System.out.println();
+                    System.out.println("Senha incorreta");
+                    numeroTentativas = numeroTentativas + 1;
+//                    numeroTentativas+=1;
+                }
+            } else if (nomeUsuario.equalsIgnoreCase(usuarioAluno)) {
+                if (senha.equals(senhaAluno)) {
+                    System.out.println();
+                    System.out.println("Bem-vindo, aluno");
+                    usuarioAutenticado = true;
+                } else {
+                    System.out.println();
+                    System.out.println("Senha incorreta");
+                    numeroTentativas = numeroTentativas + 1;
+                }
+            } else if (nomeUsuario.equalsIgnoreCase(usuarioProfessor)) {
+                if (senha.equals(senhaProfessor)) {
+                    System.out.println();
+                    System.out.println("Bem-vindo, professor");
+                    usuarioAutenticado = true;
+                } else {
+                    System.out.println();
+                    System.out.println("Senha incorreta");
+                    numeroTentativas = numeroTentativas + 1;
+
+                }
+            } else {
+                System.out.println();
+                System.out.println("Usuário não encontrado");
+                numeroTentativas = numeroTentativas + 1;
+            }
+
+
         }
 
-        if ( nomeUsuario.equalsIgnoreCase(usuarioAdmin)
-                && (senha.equals(senhaAdmin))) {
+        if (numeroTentativas >= limiteTentativas)
+            System.out.println("Acesso bloqueado. Tente novamente mais tarde.");
 
-        }
+
     }
 }
+
+
+//        if (nomeUsuario.equalsIgnoreCase(usuarioAdmin)
+//                && (senha.equals(senhaAdmin))) {
+//
+//        } else if (nomeUsuario.equalsIgnoreCase(usuarioAluno)
+//                && (senha.equals(senhaAdmin))) {
+//
+//        } else if (nomeUsuario.equalsIgnoreCase(usuarioProfessor)
+//                && (senha.equals(senhaAdmin))) {
+//
+//        }
